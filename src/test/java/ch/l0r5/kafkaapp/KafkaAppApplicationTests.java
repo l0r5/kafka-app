3586deb1fdbff1b81d7063923766a9eea5301dc3
l0r5 <lars-braitinger@web.de>
2020-07-17 13:47:10 +0200

    Initial commit

---
+++ src/test/java/ch/l0r5/kafkaapp/KafkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.l0r5.kafkaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
