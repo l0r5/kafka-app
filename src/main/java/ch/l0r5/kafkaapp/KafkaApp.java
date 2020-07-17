b7490dee7465a6cd856f2a0d823e30d22dcb82eb
l0r5 <lars-braitinger@web.de>
2020-07-17 15:27:57 +0200

    Rename to KafkaApp

---
+++ src/main/java/ch/l0r5/kafkaapp/KafkaApp.java
@@ -0,0 +1,19 @@
+package ch.l0r5.kafkaapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApp {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(KafkaApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApp.class, args);
+
+        LOGGER.info("Hello World {}","test");
+    }
+
+}
